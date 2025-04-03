1547ef5e84a8de34e4a54a4a87cc41f99298ab2d
Jack <jack282323@gmail.com>
2025-04-03 14:56:46 +0200

    Fixed navbar for mobile view

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { Link as ScrollLink } from "react-scroll";
-import { Bars3Icon } from "@heroicons/react/24/solid";
 
-const Header = () => {
+function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
   const [navbar, setNavbar] = useState(false);
-
   useEffect(() => {
     const handleScroll = () => {
       const heroSection = document.getElementById("hero");
@@ -25,109 +22,106 @@ const Header = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
   return (
-    <header
-      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
-        isScrolled
-          ? "bg-white shadow-md text-gray-800"
-          : "bg-transparent text-white"
-      }`}
-    >
-      <div className="container mx-auto flex justify-between items-center">
-        <h1
-          className="${
+    <div>
+      <nav className="w-full bg-black fixed top-0 left-0 right-0 z-50">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 text-white">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              {/* LOGO */}
+              <Link href="/">
+                <h2 className="text-2xl text-cyan-600 font-bold ">LOGO</h2>
+              </Link>
+              {/* HAMBURGER BUTTON FOR MOBILE */}
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-white rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <Image src="/close.png" width={30} height={30} alt="logo" />
+                  ) : (
+                    <Image
+                      src="/menu.png"
+                      width={30}
+                      height={30}
+                      alt="logo"
+                      className="focus:border-none active:border-none"
+                    />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? "p-12 md:p-0 block" : "hidden"
+              }`}
+            >
+              <ul className="h-screen md:h-auto items-center justify-center md:flex gap-5">
+                <li>
+                  <Link
+                    href="/"
+                    className="${
               isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
-            }`} text-xl font-bold z-20"
-        >
-          Your SaaS
-        </h1>
-        <div className="md:hidden">
-          <button onClick={() => setNavbar(!navbar)}>
-            {navbar ? (
-              <Image
-                src="/close.png"
-                width={30}
-                height={30}
-                alt="close-button"
-              />
-            ) : (
-              <Image src="/more.png" width={30} height={30} alt="more-button" />
-            )}
-          </button>
+            }`} hover:text-[#cc7af3]"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <ScrollLink
+                    to="tech"
+                    smooth={true}
+                    className="cursor-pointer ${
+              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
+            }`} hover:text-[#cc7af3]"
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    Tech
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    to="projects"
+                    smooth={true}
+                    className="cursor-pointer ${
+              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
+            }`} hover:text-[#cc7af3]"
+                  >
+                    Projects
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    to="about"
+                    smooth={true}
+                    className="cursor-pointer ${
+              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
+            }`} hover:text-[#cc7af3]"
+                  >
+                    About
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    to="contact"
+                    smooth={true}
+                    className="cursor-pointer ${
+              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
+            }`} hover:text-[#cc7af3]"
+                  >
+                    Contact
+                  </ScrollLink>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <ul className="flex space-x-4 items-center">
-          <li>
-            <Link
-              href="/"
-              className="${
-              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
-            }`} hover:text-[#cc7af3]"
-            >
-              Home
-            </Link>
-          </li>
-
-          <li>
-            <ScrollLink
-              to="tech"
-              smooth={true}
-              className="cursor-pointer ${
-              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
-            }`} hover:text-[#cc7af3]"
-              onClick={() => setNavbar(!navbar)}
-            >
-              Tech
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="projects"
-              smooth={true}
-              className="cursor-pointer ${
-              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
-            }`} hover:text-[#cc7af3]"
-            >
-              Projects
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="about"
-              smooth={true}
-              className="cursor-pointer ${
-              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
-            }`} hover:text-[#cc7af3]"
-            >
-              About
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="contact"
-              smooth={true}
-              className="cursor-pointer ${
-              isScrolled ? 'text-gray-700 hover:text-[#cc7af3]' : 'text-white hover:text-[#cc7af3]'
-            }`} hover:text-[#cc7af3]"
-            >
-              Contact
-            </ScrollLink>
-          </li>
-        </ul>
-        <div className="md:hidden">
-          <button
-            onClick={() => {
-              setIsOpen(!isOpen);
-            }}
-            type="button"
-            className="fill-gray-100"
-          >
-            <Bars3Icon className="h-6 w-6 text-white" />
-          </button>
-        </div>
-      </div>
-    </header>
+      </nav>
+    </div>
   );
-};
+}
 
-export default Header;
+export default NavBar;
