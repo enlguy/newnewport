cd255670023760003a7d781a18e743abca3cca5c
Jack <jack282323@gmail.com>
2025-10-01 13:36:58 +0200

    Fixing GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import { Bad_Script, Geist, Geist_Mono, Neonderthaw } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,10 +36,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const GA_MEASUREMENT_ID = 'G-X0SZWST34Y'
   return (
     <html lang="en">
-      {/* Conditionally render the GA component in production only */}
-      {process.env.NODE_ENV === "production" && <GoogleAnalytics />}
+      <head>
+      <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
