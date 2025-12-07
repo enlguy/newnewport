a8da93cdf32e02214997a6815d4773ce56b1859e
Jack <jack282323@gmail.com>
2025-12-07 13:25:14 +0100

    Trying to get Netlify to load dynamic images still

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Force Next.js to generate the image optimizer properly
+  images: {
+    unoptimized: false,
+    loader: "default",
+  },
+
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.lottie$/,
